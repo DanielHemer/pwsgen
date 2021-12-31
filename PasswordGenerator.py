2ac50c22de4dcf80d878ca27c9c1de6a1af201a7
DanielHemer <89794499+DanielHemer@users.noreply.github.com>
2022-01-01 06:51:02 +1030

    File Uploaded

---
+++ PasswordGenerator.py
@@ -0,0 +1,19 @@
+import random
+
+print('Password Generator by Daniel Hemer')
+
+chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*().,?0123456789'
+
+number = input('Amount of passwords needed: ')
+number = int(number)
+
+length = input('your password length; ')
+length = int(length)
+
+print('\nhere are your passwords:')
+
+for pwd in range(number):
+    passwords = ''
+    for c in range(length):
+        passwords += random.choice(chars)
+    print(passwords)
